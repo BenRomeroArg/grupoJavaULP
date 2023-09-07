ce70c3f168d7e792ff02932defb936266bb10276
Arancibia Leiria Leandro Nicanor <Nicanor95@users.noreply.github.com>
2023-09-07 12:33:10 -0300

    Limpiados comentarios de main.

--- T3grupoJavaULP/src/t3grupojavaulp/T3grupoJavaULP.java
+++ T3grupoJavaULP/src/t3grupojavaulp/T3grupoJavaULP.java
@@ -1,18 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package t3grupojavaulp;
 
-/**
- *
- * @author nikan
- */
-public class T3grupoJavaULP {
 
-    /**
-     * @param args the command line arguments
-     */
+public class T3grupoJavaULP {
     public static void main(String[] args) {
         // TODO code application logic here
     }
