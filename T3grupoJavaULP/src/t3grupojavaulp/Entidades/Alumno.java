0a2260dc5e167df2986b37649915ba039e2d09ce
Arancibia Leiria Leandro Nicanor <Nicanor95@users.noreply.github.com>
2023-09-07 12:31:16 -0300

    Añadir alumno.java

--- T3grupoJavaULP/src/t3grupojavaulp/Entidades/Alumno.java
+++ T3grupoJavaULP/src/t3grupojavaulp/Entidades/Alumno.java
@@ -1,13 +1,121 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package t3grupojavaulp.Entidades;
 
-/**
- *
- * @author nikan
- */
+import java.sql.Date;
+import java.time.LocalDate;
+
 public class Alumno {
+    private int idAlumno;
+    private int dni;
+    private String nombre;
+    private String apellido;
+    private Date fechaNacimiento;
+    private boolean estado;
+
+    public Alumno(int idAlumno, int dni, String nombre, String apellido, Date fechaNacimiento, boolean estado) {
+        this.idAlumno = idAlumno;
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+        this.estado = estado;
+    }
+
+    public Alumno(int dni, String nombre, String apellido, Date fechaNacimiento, boolean estado) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+        this.estado = estado;
+    }
+
+    public Alumno() {
+    }
+
+    public int getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
     
+    /**
+     * Devuelve FechaNacimiento como LocalDate en vez de sql.Date.
+     * 
+     * @return LocalDate
+     */
+    public LocalDate getFechaNacimientoLocalDate() {
+        return fechaNacimiento.toLocalDate();
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" + "idAlumno=" + idAlumno + ", dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + ", fechaNacimiento=" + fechaNacimiento + ", estado=" + estado + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 11 * hash + this.idAlumno;
+        hash = 11 * hash + this.dni;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Alumno other = (Alumno) obj;
+        if (this.idAlumno != other.idAlumno) {
+            return false;
+        }
+        return this.dni == other.dni;
+    }
 }
