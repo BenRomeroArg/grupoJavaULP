d81104c3b970602f3d56ba19237a34fce2e4b61a
naiggace <naiggace@hotmail.com>
2023-09-08 11:02:51 -0300

    create conexion

---
+++ T3grupoJavaULP/src/t3grupojavaulp/Conexion/Conexion.java
@@ -0,0 +1,14 @@
+package t3grupojavaulp.Conexion;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author naigg
+ */
+public class Conexion {
+    
+}
