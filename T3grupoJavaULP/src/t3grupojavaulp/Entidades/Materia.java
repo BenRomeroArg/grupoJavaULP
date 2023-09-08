7d157c3e24f056bc7fb8ad788235fa64c493f2d5
naiggace <naiggace@hotmail.com>
2023-09-08 10:51:05 -0300

    alumno, materia e inscr

--- T3grupoJavaULP/src/t3grupojavaulp/Entidades/Materia.java
+++ T3grupoJavaULP/src/t3grupojavaulp/Entidades/Materia.java
@@ -2,5 +2,62 @@ package t3grupojavaulp.Entidades;
 
 
 public class Materia {
+    private int idMateria;
+    private String nombre;
+    private int anioMateria;
+    private boolean activo;
+
+    public Materia() {
+    }
+
+    public Materia(String nombre, int anioMateria, boolean activo) {
+        this.nombre = nombre;
+        this.anioMateria = anioMateria;
+        this.activo = activo;
+    }
+
+    public Materia(int idMateria, String nombre, int anioMateria, boolean activo) {
+        this.idMateria = idMateria;
+        this.nombre = nombre;
+        this.anioMateria = anioMateria;
+        this.activo = activo;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAnioMateria() {
+        return anioMateria;
+    }
+
+    public void setAnioMateria(int anioMateria) {
+        this.anioMateria = anioMateria;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    @Override
+    public String toString() {
+        return "Materia{" + "idMateria=" + idMateria + ", nombre=" + nombre + ", anioMateria=" + anioMateria + '}';
+    }
     
 }
