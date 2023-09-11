47fa2193fbf04407188f826be5db3b12843db6b9
Arancibia Leiria Leandro Nicanor <Nicanor95@users.noreply.github.com>
2023-09-11 11:11:27 -0300

    Add MateriaData skeleton

--- T3grupoJavaULP/src/t3grupojavaulp/accesoADatos/MateriaData.java
+++ T3grupoJavaULP/src/t3grupojavaulp/accesoADatos/MateriaData.java
@@ -1,6 +1,33 @@
 package t3grupojavaulp.accesoADatos;
 
+import java.sql.Connection;
+import t3grupojavaulp.Entidades.Materia;
+
 
 public class MateriaData {
+    private Connection con;
+
+    public MateriaData() {
+        con = Conexion.conectar();
+    }
     
+    public void guardarMateria(Materia materia) {
+        // TODO
+    }
+    
+    public Materia buscarMateria(int id) {
+        // TODO
+    }
+    
+    public void modificarMateria(Materia materia) {
+        // TODO
+    }
+    
+    public void eliminarMateria(int id) {
+        // TODO
+    }
+    
+    public ArrayList<Materia> listarMaterias() {
+        // TODO
+    }
 }
