e01771fd9a1c5aca28614dca6fd35c48316ecda2
Arancibia Leiria Leandro Nicanor <Nicanor95@users.noreply.github.com>
2023-09-11 12:28:20 -0300

    a

--- T3grupoJavaULP/src/t3grupojavaulp/accesoADatos/InscripcionData.java
+++ T3grupoJavaULP/src/t3grupojavaulp/accesoADatos/InscripcionData.java
@@ -7,48 +7,53 @@ import t3grupojavaulp.Entidades.Alumno;
 import t3grupojavaulp.Entidades.Inscripcion;
 
 public class InscripcionData {
-    
+
     Connection con;
     MateriaData matData;
     AlumnoData aluData;
 
     public InscripcionData() {
-         con = Conexion.conectar();
+        con = Conexion.conectar();
     }
-      public void guardarInscripcion(Inscripcion insc) {
+
+    public void guardarInscripcion(Inscripcion insc) {
         // TODO
     }
-    
+
     public void eliminarMateria(int id) {
         // TODO
     }
-    
+
     public ArrayList<Inscripcion> obtenerInscripciones() {
         // TODO
         return null;
-         }
-       public ArrayList<Inscripcion> obtenerInscripcionesPorAlumno(int id) {
+    }
+
+    public ArrayList<Inscripcion> obtenerInscripcionesPorAlumno(int id) {
         // TODO
         return null;
-         }
-       public ArrayList<Materia> obtenerMateriasCursadas(int id) {
+    }
+
+    public ArrayList<Materia> obtenerMateriasCursadas(int id) {
         // TODO
         return null;
-         }
-           public ArrayList<Materia> obtenerMateriasNOCursadas(int id) {
+    }
+
+    public ArrayList<Materia> obtenerMateriasNOCursadas(int id) {
         // TODO
         return null;
-         }
-       public void borrarInscripcionMateriaAlumno(int idAlumno, int idMateria) {
-       }
-       public void actualizarNota(int idAlumno, int idMateria, double nota){
-       
-       }
-         public ArrayList<Alumno> obtenerAlumnosXMateria(int idMateria) {
+    }
+
+    public void borrarInscripcionMateriaAlumno(int idAlumno, int idMateria) {
+    }
+
+    public void actualizarNota(int idAlumno, int idMateria, double nota) {
+
+    }
+
+    public ArrayList<Alumno> obtenerAlumnosXMateria(int idMateria) {
         // TODO
         return null;
-         }
-       
+    }
+
 }
-    
-    
