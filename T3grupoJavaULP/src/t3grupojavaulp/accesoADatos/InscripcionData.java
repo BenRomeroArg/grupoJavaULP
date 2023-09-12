74be138b61fc8b5712a42981f715511e23016782
Ben <100096813+BenRomeroArg@users.noreply.github.com>
2023-09-12 09:56:43 -0300

    inscripcionData-guardar
    guardar inscripción

--- T3grupoJavaULP/src/t3grupojavaulp/accesoADatos/InscripcionData.java
+++ T3grupoJavaULP/src/t3grupojavaulp/accesoADatos/InscripcionData.java
@@ -1,7 +1,13 @@
 package t3grupojavaulp.accesoADatos;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
 import t3grupojavaulp.Entidades.Materia;
 import t3grupojavaulp.Entidades.Alumno;
 import t3grupojavaulp.Entidades.Inscripcion;
@@ -18,6 +24,24 @@ public class InscripcionData {
 
     public void guardarInscripcion(Inscripcion insc) {
         // TODO
+        String sql = "INSERT INTO `inscripcion`( `nota`, `idAlumno`, `idMateria`) VALUES (?,?,?)";
+        
+        try {
+                PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+                ps.setDouble(1, insc.getNota());
+                ps.setInt(2, insc.getAlumno().getIdAlumno());
+                ps.setInt(3, insc.getMateria().getIdMateria());
+                ps.executeUpdate();
+                ResultSet res = ps.getGeneratedKeys();
+                if(res.next()){
+                    JOptionPane.showMessageDialog(null, "Se guardo la inscripcion con exito!");
+                }
+            ps.close();
+            
+        } catch (SQLException ex) {
+           JOptionPane.showMessageDialog(null, "Error SQL");
+        }
+        
     }
 
     public void eliminarMateria(int id) {
