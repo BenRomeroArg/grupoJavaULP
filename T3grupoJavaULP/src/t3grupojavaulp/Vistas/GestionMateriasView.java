8d9034b499e42463dda94cfc9836f800ec33a04f
naiggace <naiggace@hotmail.com>
2023-09-19 12:52:40 -0300

    Update GestionMateriasView.java

--- T3grupoJavaULP/src/t3grupojavaulp/Vistas/GestionMateriasView.java
+++ T3grupoJavaULP/src/t3grupojavaulp/Vistas/GestionMateriasView.java
@@ -178,7 +178,8 @@ public class GestionMateriasView extends javax.swing.JInternalFrame {
     }//GEN-LAST:event_jbEstadoActionPerformed
 
     private void jbNuevoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbNuevoActionPerformed
-        
+        //AGREGAR MATERIA NUEVA
+
         String nombre = jtNombre.getText();
         int año = Integer.parseInt(jtAnio.getText());
         boolean estado = jbEstado.isSelected();
