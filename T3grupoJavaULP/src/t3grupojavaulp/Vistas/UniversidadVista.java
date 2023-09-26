990c10aac3ec02959fbb816fca510d56eb869564
naiggace <naiggace@hotmail.com>
2023-09-26 10:29:48 -0300

    removeall

--- T3grupoJavaULP/src/t3grupojavaulp/Vistas/UniversidadVista.java
+++ T3grupoJavaULP/src/t3grupojavaulp/Vistas/UniversidadVista.java
@@ -164,7 +164,7 @@ public class UniversidadVista extends javax.swing.JFrame {
     }//GEN-LAST:event_jmForAlumnoActionPerformed
 
     private void jManejoInscActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jManejoInscActionPerformed
-    //    Escritorio.removeAll();
+        Escritorio.removeAll();
         Escritorio.repaint();
         GestionInscripcionView giv = new GestionInscripcionView();
         giv.setVisible(true);
@@ -173,7 +173,7 @@ public class UniversidadVista extends javax.swing.JFrame {
     }//GEN-LAST:event_jManejoInscActionPerformed
 
     private void jMForMateriaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMForMateriaActionPerformed
-    //    Escritorio.removeAll();
+        Escritorio.removeAll();
         Escritorio.repaint();
         GestionMateriasView gmv = new GestionMateriasView();
         gmv.setVisible(true);
@@ -182,7 +182,7 @@ public class UniversidadVista extends javax.swing.JFrame {
     }//GEN-LAST:event_jMForMateriaActionPerformed
 
     private void jOrdenarAlumnosMateriaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jOrdenarAlumnosMateriaActionPerformed
-    //    Escritorio.removeAll();
+        Escritorio.removeAll();
         Escritorio.repaint();
         ConsultaAlumnoPorMateriaView camV = new ConsultaAlumnoPorMateriaView();
         camV.setVisible(true);
@@ -191,7 +191,7 @@ public class UniversidadVista extends javax.swing.JFrame {
     }//GEN-LAST:event_jOrdenarAlumnosMateriaActionPerformed
 
     private void jManipNotasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jManipNotasActionPerformed
-    //    Escritorio.removeAll();
+        Escritorio.removeAll();
         Escritorio.repaint();
         ManipulacionNotasView camV = new ManipulacionNotasView();
         camV.setVisible(true);
